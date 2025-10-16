07ca981cfa9737e05b5d60c1ae4aa18bf62034df
Chayan Kumawat <130827975+Notchayan@users.noreply.github.com>
2025-10-16 16:46:51 +0530

    Refactor Sum of Two Values solution for clarity

--- CSES/Sorting and Searching/Sum of Two Values.cpp
+++ CSES/Sorting and Searching/Sum of Two Values.cpp
@@ -4,15 +4,23 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    long long n,x; 
-    unordered_map<long long,int> m;
-    for(int i=1;i<=n;i++){
-        long long a; cin>>a;
-        auto it=m.find(x-a);
-        if(it!=m.end()){
-            cout<<it->second<<" "<<i<<"\n";
+
+    long long n, x;
+    if(!(cin >> n >> x)) return 0;
+
+    unordered_map<long long, int> pos;
+    pos.reserve(n * 2);
+
+    for(int i = 1; i <= n; i++){
+        long long a; 
+        cin >> a;
+        auto it = pos.find(x - a);
+        if(it != pos.end()){
+            cout << it->second << " " << i << "\n";
             return 0;
         }
-        if(!m.count(a)) m[a]=i;
+        if(!pos.count(a)) pos[a] = i;
     }
-    cout<<"IMPOSSIBLE\n";
+    cout << "IMPOSSIBLE\n";
+    return 0;
+}
