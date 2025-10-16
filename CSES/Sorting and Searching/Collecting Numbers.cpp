3bb8da4c1577d1f3d844733271ef9f08aff5ddf8
Chayan Kumawat <130827975+Notchayan@users.noreply.github.com>
2025-10-16 21:34:36 +0530

    Add Collecting Numbers solution in C++

---
+++ CSES/Sorting and Searching/Collecting Numbers.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; 
+    cin >> n;
+    vector<int> pos(n+1);
+    for(int i=1;i<=n;i++){
+        int x; cin >> x;
+        pos[x]=i;
+    }
+    int ans=1;
+    for(int i=1;i<n;i++) if(pos[i]>pos[i+1]) ans++;
+    cout << ans << "\n";
+    return 0;
+}
